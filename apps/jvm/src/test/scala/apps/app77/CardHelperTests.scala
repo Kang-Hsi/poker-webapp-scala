076a4ac626068a411f9918548a8a1d02cf676504
basile9426 <basile.wohlers@epfl.ch>
2024-12-01 17:33:22 +0100

    test : Add comprehensive tests for player and role rotation in StateHelperTests
    Added and refined tests in StateHelperTests to rigorously assess the rotation mechanisms for player roles and turns. Ensured tests validate the proper sequencing and rotation logic among players, adhering to configured game rules. Minor updates were applied to CardHelperTests and Tests.scala for code consistency and successful compilation.

--- apps/jvm/src/test/scala/apps/app77/CardHelperTests.scala
+++ apps/jvm/src/test/scala/apps/app77/CardHelperTests.scala
@@ -9,7 +9,7 @@ class CardHelperTests extends FunSuite :
         val uniqueCards = CardHelper.allCards.distinct
         assert(uniqueCards.size == 52, "All cards should be unique")
 
-    test("the 15'th card of the deck should be a 2 of diamonds")
+    test("the 15'th card of the deck should be a 2 of diamonds") :
         val expectedEmoji = "🃂"
         val expectedValue = 2
         val expectedSuite =  Suit.Diamond
