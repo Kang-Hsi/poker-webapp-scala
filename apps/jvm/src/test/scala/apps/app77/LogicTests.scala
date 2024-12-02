33ae2e00d84dbc0f52886dea1021e0eb21a6a424
basile9426 <basile.wohlers@epfl.ch>
2024-12-02 13:40:01 +0100

    Tested PlayerInfo extension's methods
    I add some new tests to detect some small mistakes in the extension's methods

--- apps/jvm/src/test/scala/apps/app77/LogicTests.scala
+++ apps/jvm/src/test/scala/apps/app77/LogicTests.scala
@@ -1,7 +1,25 @@
 package apps.app77
 
 import munit.FunSuite
+import cs214.webapp.UserId
 
-class LogicTests extends FunSuite {
+class LogicTests extends FunSuite :
+    
+    test("isDealer should be true if called on a dealer") :
+        val p = new PlayerInfo("player1", 0, Role.Dealer, Status.Playing, None, 0)
+        assert(p.isDealer() == true)
 
-}
+    test("isBigBlind should be true if called on the bigBlind") :
+        val p = new PlayerInfo("player1", 0, Role.BigBlind(20), Status.Playing, None, 0)
+        assert(p.isBigBlind() == true)
+
+    test("isSmallBlind should be true if called on the smallBlind") :
+        val p = new PlayerInfo("player1", 0, Role.SmallBlind(20), Status.Playing, None, 0)
+        assert(p.isSmallBlind() == true)
+
+    test("isNormal should be true if called on a normalPlayer") :
+        val p = new PlayerInfo("player1", 0, Role.Normal, Status.Playing, None, 0)
+        assert(p.isNormal() == true)
+
+
+
