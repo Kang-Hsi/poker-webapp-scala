2fd35ada75b166ba07065d699d248893725ea63f
basile9426 <basile.wohlers@epfl.ch>
2024-12-02 19:31:35 +0100

    Wires : Event encoding and decoding
    Changed the encoding and decoding event to manage the serialization by hand and not with upickle anymore.

--- apps/shared/src/main/scala/apps/app77/Wires.scala
+++ apps/shared/src/main/scala/apps/app77/Wires.scala
@@ -12,11 +12,28 @@ object Wire extends AppWire[Event, View]:
   import ujson.*
 
   override object eventFormat extends WireFormat[Event] :
-    override def encode(event: Event): Value = upickle.default.write(event)
-    override def decode(json: Value): Try[Event] = Try(upickle.default.read[Event](json))
+    override def encode(event: Event): Value = event match
+      case Check() => ujson.Obj("type" -> "check")
+      case Fold()  => ujson.Obj("type" -> "fold")
+      case Bet(amount) => ujson.Obj("type" -> "bet", "amount" -> ujson.Num(amount))
+
+    override def decode(json: Value): Try[Event] = Try {
+      val obj = json.obj
+      obj("type").str match
+        case "check" => Event.Check()
+        case "fold"  => Event.Fold()
+        case "bet"   =>
+          val amount = obj("amount").num.toInt
+          Event.Bet(amount)
+    }
 
   override object viewFormat extends WireFormat[View] :
     override def encode(view: View): Value = upickle.default.write(view)
     override def decode(json: Value): Try[View] = Try(upickle.default.read[View](json))
 
+
+    /* enum Event derives ReadWriter:
+  case Check()
+  case Fold()
+  case Bet(amount:Money) */
 
