80d86cf2dd913b5ecd4ea4432a8debe7ad6cdbc5
basile9426 <basile.wohlers@epfl.ch>
2024-12-15 11:15:00 +0100

    Wires : ScalaDoc
    Added ScalaDoc comments for the Event and View serializers.

--- apps/shared/src/main/scala/apps/app77/Wires.scala
+++ apps/shared/src/main/scala/apps/app77/Wires.scala
@@ -10,13 +10,33 @@ object Wire extends AppWire[Event, View]:
   import View.*
   import ujson.*
 
+  /**
+    * Object provided to encode and decode an event.
+    */
   override object eventFormat extends WireFormat[Event] :
+
+    /**
+      * Provides a JSON encoding of an Event.
+      * Converts an Event instance into a JSON object that represents the event type and its associated data.
+      *
+      * @param event the Event instance to encode
+      * @return the JSON representation of the event
+      */
     override def encode(event: Event): Value = event match
 
       case Check() => ujson.Obj("type" -> "check")
       case Fold()  => ujson.Obj("type" -> "fold")
       case Bet(amount) => ujson.Obj("type" -> "bet", "amount" -> ujson.Num(amount))
 
+    /**
+      * Decodes a JSON object into an Event instance.
+      * Attempts to convert a JSON object into an Event
+      * by extracting the type of the event and any necessary data.
+      *
+      * @param json the JSON value of the event
+      * @return A Try instance encapsulating the View, either Success with the View
+      * or Failure with an exception if any part fails to decode correctly.
+      */
     override def decode(json: Value): Try[Event] = Try {
       val obj = json.obj
       obj("type").str match
@@ -28,11 +48,29 @@ object Wire extends AppWire[Event, View]:
     }
 
   override object viewFormat extends WireFormat[View] :
+
+    /**
+      * Encodes a View into a JSON object.
+      * Consolidates various aspects of the game view into a JSON array
+      * containing game information, configuration, and phase.
+      *
+      * @param view the View instance to encode
+      * @return the JSON representation of the view
+      */
     override def encode(view: View): Value = ujson.Arr(
       viewFormatHelpers.encodeGameInfo(view.gameInfo),
       viewFormatHelpers.encodeGameConfig(view.gameConfig),
       viewFormatHelpers.encodeGamePhase(view.gamePhase)
       )
+    /**
+      * Decodes a JSON array into a View instance.
+      * Extracts game information, configuration, and phase from a JSON array
+      * and combines them into a View.
+      *
+      * @param json the JSON array representing the view
+      * @return A Try instance encapsulating the View, either Success with the View
+      * or Failure with an exception if any part fails to decode correctly.
+      */
     override def decode(json: Value): Try[View] = Try{
       val arr = json.arr
       val gameInfo = viewFormatHelpers.decodeGameInfo(arr(0)).get
